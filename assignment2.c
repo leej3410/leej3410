6ae39ca94d67c478dd5a6bbb338ebac8fd6246d0
leej3410 <jaehun@jaehun-iui-MacBook-Pro.local>
2017-03-06 20:54:43 -0600

    file open
    pointer file read text file and if read is null will return 1

--- assignment2.c
+++ assignment2.c
@@ -6,4 +6,20 @@
 
  
 int main(void) {
+
+ FILE *read;//, *write;
+   //char ch;
+ char line[100];// need to give a right size
+
+ read = fopen("infile.txt", "r");
+
+   if(read == NULL){
+
+    printf("FAIL");
+    return 1;
+   }   
+
+
 }
+
+
