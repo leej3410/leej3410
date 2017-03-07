b3a7741d4d6c56474089e8eb45f836534f31c609
leej3410 <jaehun@jaehun-iui-MacBook-Pro.local>
2017-03-06 21:05:00 -0600

    print top and bottom
    print + and - top and bottom same as the number of rows and cols

--- assignment2.c
+++ assignment2.c
@@ -27,6 +27,15 @@ fscanf(read , "%s %s %s %s %s %d %d %d", c1 , c2 , c3 , c4 , c5 , &rows , &cols 
 
    while(fgets(line , sizeof(line)/sizeof(line[0]) , read) != NULL){
 
+   	for(int hypen = 0; hypen < rows; hypen++){
+      if(hypen == 0){
+        printf("+");
+      }
+       if(hypen <= rows){
+        printf("-");
+       }
+     }printf("+\n");
+
    	for(int i = 0; i < rows; i ++){
       for(int j = 0; j <= cols; j++){
       	fscanf(read , "%c" , &string[i][j]);
